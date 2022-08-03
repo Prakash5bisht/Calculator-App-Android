e563a8d5ed644e2e4e4cd39647aa0f125bee14bb
Prakash <singhbprakash505@gmail.com>
2022-08-03 21:11:31 +0530

    * used synthetic for view binding *fixed landscape layout

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val OPERATOR = "operator"
+private const val OPERAND = "operand"
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,29 +26,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        result = findViewById(R.id.result)
-        newNumber = findViewById(R.id.newNumber)
-        displayOperation = findViewById(R.id.operation)
+//        result = findViewById(R.id.result)
+//        newNumber = findViewById(R.id.newNumber)
+//        displayOperation = findViewById(R.id.operation)
 
 
-        val button0 = findViewById<Button>(R.id.button0)
-        val button1 = findViewById<Button>(R.id.button1)
-        val button2 = findViewById<Button>(R.id.button2)
-        val button3 = findViewById<Button>(R.id.button3)
-        val button4 = findViewById<Button>(R.id.button4)
-        val button5 = findViewById<Button>(R.id.button5)
-        val button6 = findViewById<Button>(R.id.button6)
-        val button7 = findViewById<Button>(R.id.button7)
-        val button8 = findViewById<Button>(R.id.button8)
-        val button9 = findViewById<Button>(R.id.button9)
-        val buttonDot = findViewById<Button>(R.id.buttonDot)
+//        val button0 = findViewById<Button>(R.id.button0)
+//        val button1 = findViewById<Button>(R.id.button1)
+//        val button2 = findViewById<Button>(R.id.button2)
+//        val button3 = findViewById<Button>(R.id.button3)
+//        val button4 = findViewById<Button>(R.id.button4)
+//        val button5 = findViewById<Button>(R.id.button5)
+//        val button6 = findViewById<Button>(R.id.button6)
+//        val button7 = findViewById<Button>(R.id.button7)
+//        val button8 = findViewById<Button>(R.id.button8)
+//        val button9 = findViewById<Button>(R.id.button9)
+//        val buttonDot = findViewById<Button>(R.id.buttonDot)
 
 
-        val buttonEqual = findViewById<Button>(R.id.buttonEqual)
-        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
-        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
-        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
-        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
+//        val buttonEqual = findViewById<Button>(R.id.buttonEqual)
+//        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+//        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+//        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
+//        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
 
 
         val listener = View.OnClickListener { view ->
@@ -120,6 +125,23 @@ class MainActivity : AppCompatActivity() {
         result.setText(operand1.toString())
         newNumber.setText("")
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        if(operand1 != null){
+            outState.putDouble(OPERAND, operand1!!)
+        }
+        outState.putString(OPERATOR, pendingOperation)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        operand1 = savedInstanceState.getDouble(OPERAND)
+        pendingOperation = savedInstanceState.getString(OPERATOR).toString()
+
+        displayOperation.text = pendingOperation
     }
 
 }
